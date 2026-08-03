94d7ecf36c43065bb099e52b5d2512d0fbfc7dae
namsoon00 <namsoon00@gmail.com>
2026-08-03 15:35:45 +0900

    chore: publish running result review

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,7 +43,7 @@ const removeLoadingIndicator = () => {
   }
 };
 
-const webAssetVersion = 'cfd9ac8bee2c';
+const webAssetVersion = '9ff8c09eaaab';
 const unstampedAssetVersionToken = ['__WEB', 'ASSET', 'VERSION__'].join('_');
 const legacyCleanupReloadKey = 'football-note:legacy-flutter-cleanup-reload:v8';
 const webBuildReloadKeyPrefix = 'football-note:web-build-reload:v1';
