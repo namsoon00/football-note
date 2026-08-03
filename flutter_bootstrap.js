09ddc140e44270eb2b98eed74bc534ea959c3c28
namsoon00 <namsoon00@gmail.com>
2026-08-03 22:06:59 +0900

    fix: remove broken running cycle autoplay (#0)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,7 +43,7 @@ const removeLoadingIndicator = () => {
   }
 };
 
-const webAssetVersion = 'cca83e84c7f2';
+const webAssetVersion = '341151ef48ac';
 const unstampedAssetVersionToken = ['__WEB', 'ASSET', 'VERSION__'].join('_');
 const legacyCleanupReloadKey = 'football-note:legacy-flutter-cleanup-reload:v8';
 const webBuildReloadKeyPrefix = 'football-note:web-build-reload:v1';
