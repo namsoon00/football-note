db71ade41ddd3678e6342818f378bcaf4e03c1fa
namsoon00 <namsoon00@gmail.com>
2026-08-03 11:22:47 +0900

    chore: publish running evidence guide web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,7 +43,7 @@ const removeLoadingIndicator = () => {
   }
 };
 
-const webAssetVersion = 'bd883689e2f7';
+const webAssetVersion = 'cfd9ac8bee2c';
 const unstampedAssetVersionToken = ['__WEB', 'ASSET', 'VERSION__'].join('_');
 const legacyCleanupReloadKey = 'football-note:legacy-flutter-cleanup-reload:v8';
 const webBuildReloadKeyPrefix = 'football-note:web-build-reload:v1';
